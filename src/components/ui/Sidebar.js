f614ac734f98bd8d55eba5d6d3e3f3d5fb0050db
mayakaconard <mayakaconard@gmail.com>
2018-09-10 16:45:33 +0300

    Updated sidebar

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -41,6 +41,10 @@ class Sidebar extends React.Component {
         <p>This is the collapsible content. It can be any element or React component you like.</p>
         <p>It can even be another Collapsible component. Check out the next section!</p>
       </Collapsible>
+      <Collapsible trigger="Programs">
+        <p>This is the collapsible content. It can be any element or React component you like.</p>
+        <p>It can even be another Collapsible component. Check out the next section!</p>
+      </Collapsible>
 
       {/* Start of side Menu */}
       {/* <Accordion>
